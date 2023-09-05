8bcdd2702ec0e1e262cb510897ed31188288d895
student-studies-everyday <student.studies.everyday@gmail.com>
2023-09-05 21:41:47 +0900

    commit

--- office.bat
+++ office.bat
@@ -1,12 +1,3 @@
-cd /d e:\
-cd kc2023-tutor\retype
-npx retype start
-pause
-npx retype build
-pause
 git add .
-pause
-git commit -m "수정완료"
-pause
+git commit -m "commit"
 git push origin main
-pause
